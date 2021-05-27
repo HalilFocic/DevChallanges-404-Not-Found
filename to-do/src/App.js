e280f4da3670c7fd753805c014c3a2720f328735
Halil Focic <halil.focic@gmail.com>
2021-05-27 12:25:03 +0200

    title,slider & input

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">tu sam</div>;
+  return (
+    <div className="App">
+      <div className="app-title">#todo</div>
+
+      <div className="app-slider">
+        <div className="slider-item">
+          <div className="slider-text">All</div>
+        </div>
+        <div className="slider-item">
+          <div className="slider-text">Active</div>
+        </div>
+        <div className="slider-item">
+          <div className="slider-text">Completed</div>
+        </div>
+      </div>
+      <div className="app-form">
+        <input className="app-input"></input>
+        <button className="add-btn">Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
