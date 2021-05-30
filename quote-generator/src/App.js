0b9328ef26f1d941afa3299c6c82c30f2780f47a
Halil Focic <halil.focic@gmail.com>
2021-05-30 09:55:16 +0200

    functionality finished

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -3,8 +3,11 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 function App() {
   const [currentQuote, setCurrentQuote] = useState({});
-  const [singleQuote, setSingleQuote] = useState(false);
+  const [singleQuote, setSingleQuote] = useState(true);
+  const [multipleQuotes, setMultipleQuotes] = useState([]);
+  const [displayAll, setDisplayAll] = useState(false);
   const generateQuote = () => {
+    setDisplayAll(false);
     axios
       .get("https://quote-garden.herokuapp.com/api/v3/quotes/random", {
         params: {
@@ -14,6 +17,7 @@ function App() {
       .then((res) => {
         console.log(res.data.data);
         setCurrentQuote(res.data.data[0]);
+        setSingleQuote(true);
       });
   };
   const getAuthorQuotes = () => {
@@ -25,8 +29,9 @@ function App() {
         },
       })
       .then((res) => {
-        console.log(res.data.data);
-        setCurrentQuote(res.data.data[0]);
+        setMultipleQuotes(res.data.data);
+        setDisplayAll(true);
+        setSingleQuote(false);
       });
   };
   useEffect(() => {
@@ -47,20 +52,28 @@ function App() {
         random <i className="material-icons refresh-icon">autorenew</i>
       </div>
       <div className="author-top">
-        {singleQuote && currentQuote.quoteAuthor}
+        {!singleQuote && currentQuote.quoteAuthor}
       </div>
       <div className="quote-container">
         <div className="quote-text">"{currentQuote.quoteText}"</div>
-
-        <div className="quote-desc">
-          <div>
-            <div className="author">{currentQuote.quoteAuthor}</div>
-            <div className="genre">{currentQuote.quoteGenre}</div>
+        {displayAll &&
+          multipleQuotes.map((quote, index) => {
+            if (quote.quoteText === currentQuote.quoteText || index === 2) {
+              return null;
+            }
+            return <div className="quote-text">"{quote.quoteText}"</div>;
+          })}
+        {singleQuote && (
+          <div className="quote-desc">
+            <div>
+              <div className="author">{currentQuote.quoteAuthor}</div>
+              <div className="genre">{currentQuote.quoteGenre}</div>
+            </div>
+            <div className="quote-arrow" onClick={() => getAuthorQuotes()}>
+              <span class="material-icons arrow-icon">arrow_forward</span>
+            </div>
           </div>
-          <div className="quote-arrow" onClick={() => getAuthorQuotes()}>
-            <span class="material-icons arrow-icon">arrow_forward</span>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
